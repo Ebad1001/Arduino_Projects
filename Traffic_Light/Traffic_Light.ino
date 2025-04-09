9ace4bc373cfd18753d87a0c76336a217ade4a9b
Ebad1001 <ebadulislamfarooqi@gmail.com>
2025-04-09 16:06:26 +0530

    updated traffic_light code

--- Traffic_Light/Traffic_Light.ino
+++ Traffic_Light/Traffic_Light.ino
@@ -1,58 +1,34 @@
-// C++ code
-//
+int led_red = 11;
+int led_ylw = 12;
+int led_grn = 13;
+
 void setup()
 {
-  pinMode(9, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(4, OUTPUT);
+  // set the pin modes to OUTPUT
+  pinMode(led_red, OUTPUT);
+  pinMode(led_ylw, OUTPUT);
+  pinMode(led_grn, OUTPUT);
 
-  digitalWrite(9, LOW);
-  digitalWrite(3, LOW);
-  digitalWrite(5, LOW);
+  // initialize the pins as LOW
+  digitalWrite(led_red, LOW);
+  digitalWrite(led_ylw, LOW);
+  digitalWrite(led_grn, LOW);  
 }
 
 void loop()
 {
-  digitalWrite(8, HIGH);
-  delay(3000); // Wait for 3000 millisecond(s)
-  digitalWrite(8, LOW);
-  digitalWrite(2, HIGH);
-  delay(3000); // Wait for 3000 millisecond(s)
-  digitalWrite(2, LOW);
-  digitalWrite(4, HIGH);
-  delay(3000); // Wait for 3000 millisecond(s)
-  digitalWrite(4, LOW);
-}
-
-void setup() {
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(13, OUTPUT);
-}
-
-void loop() {
-  digitalWrite(10, HIGH);
-  digitalWrite(11, LOW);
-  digitalWrite(12, HIGH);
-  digitalWrite(13, LOW);
-  delay(1000);  // Delay a little bit to improve simulation performance
-  digitalWrite(10, LOW);
-  digitalWrite(11, HIGH);
-  digitalWrite(12, HIGH);
-  digitalWrite(13, LOW);
-  delay(1000);  // Delay a little bit to improve simulation performance
-  digitalWrite(10, HIGH);
-  digitalWrite(11, LOW);
-  digitalWrite(12, LOW);
-  digitalWrite(13, HIGH);
-  delay(1000);  // Delay a little bit to improve simulation performance
-  digitalWrite(10, LOW);
-  digitalWrite(11, HIGH);
-  digitalWrite(12, LOW);
-  digitalWrite(13, HIGH);
-  delay(1000);
+  // glow the led_red for 5 seconds
+  digitalWrite(led_red, HIGH);
+  delay(5000);
+  digitalWrite(led_red, LOW);
+  
+  // glow the led_ylw for 3 seconds
+  digitalWrite(led_ylw, HIGH);
+  delay(3000);
+  digitalWrite(led_ylw, LOW);
+  
+  // glow the led_grn for 5 seconds
+  digitalWrite(led_grn, HIGH);
+  delay(5000);
+  digitalWrite(led_grn, LOW);
 }
