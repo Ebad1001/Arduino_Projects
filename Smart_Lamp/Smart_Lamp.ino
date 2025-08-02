8e9a328aee372c8895ae65a67d7c5688fda8d0eb
Ebad1001 <ebadulislamfarooqi@gmail.com>
2025-08-02 21:16:48 +0530

    updated smart_lamp

--- Smart_Lamp/Smart_Lamp.ino
+++ Smart_Lamp/Smart_Lamp.ino
@@ -1,49 +1,37 @@
-// int LDR = 7;
-// int LED = 13;
-
-// void setup() {
-//   pinMode(LDR, INPUT);
-//   pinMode(LED, OUTPUT);
-//   Serial.begin(9600);
-// }
-
-// void loop() {
-//   // take input from LDR sensor
-//   int value = digitalRead(LDR);
-//   Serial.println(value);
-
-//   // show output on LED lamp
-//   if (value == 0) {
-//     // High light level : Turn the LED OFF
-//     digitalWrite(LED, LOW);
-//   } else {
-//     // Low light level : Turn the LED ON
-//     digitalWrite(LED, HIGH);
-//   }
-
-//   // repeat
-//   delay(100);
-// }
-
-int LDR = 7;
-int LED = 13;
+// define the component pins
+int LDR = 7;   // connect the DO pin of LDR sensor at pin 7
+               // connect the GND pin of LDR sensor at GND
+               // connect the VCC pin of LDR sensor at 5V
+int LED = 13;  // connect the anode of LED at pin 13 and its cathode at GND
 
 void setup() {
+  // set the pin modes
   pinMode(LDR, INPUT);
   pinMode(LED, OUTPUT);
+
+  // initialize the LED pin as LOW - initially the LED is off
+  digitalWrite(LED, LOW);
+
+  // initialize the serial port
   Serial.begin(9600);
 }
 
 void loop() {
-  // take input from LDR
+  // take reading from LDR sensor
   int value = digitalRead(LDR);
+
+  // print sensor readings on Serial monitor
   Serial.println(value);
-  // show output on LED
+
+  // making decision based on sensor value
   if (value == 1) {
+    // if the value is 1 (Darkness), turn on the LED
     digitalWrite(LED, HIGH);
   } else {
+    // if the value is 0 (Brightness), turn off the LED
     digitalWrite(LED, LOW);
   }
-  
+
+  // delay for smooth performance
   delay(100);
 }
